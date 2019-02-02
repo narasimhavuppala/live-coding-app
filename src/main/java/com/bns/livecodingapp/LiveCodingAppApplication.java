e7424d4547f3cc9803e5bcc1d7bba49b6ede77ef
Narasimha <nraovuppala@gmail.com>
2019-02-02 21:47:24 +0530

    first addition

---
+++ src/main/java/com/bns/livecodingapp/LiveCodingAppApplication.java
@@ -0,0 +1,14 @@
+package com.bns.livecodingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveCodingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveCodingAppApplication.class, args);
+	}
+
+}
+
